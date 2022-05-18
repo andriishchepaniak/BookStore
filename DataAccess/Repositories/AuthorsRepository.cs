849f9f8141a54acd90e15ecf36de655814b3cd08
andriishchepaniak <andrew.shtchepanyak@gmail.com>
2022-05-18 20:54:03 +0300

    Added data access layer

---
+++ DataAccess/Repositories/AuthorsRepository.cs
@@ -0,0 +1,34 @@
+﻿using BookStoreModels;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccess.Repositories
+{
+    public class AuthorsRepository
+    {
+        private readonly BookStoreContext _db;
+        public AuthorsRepository(BookStoreContext context)
+        {
+            _db = context;
+        }
+
+        public async Task<Author> Add(Author author)
+        {
+            var existAuthor = await _db.Authors.FirstOrDefaultAsync(a => a.Name == author.Name);
+
+            if (existAuthor == null)
+            {
+                await _db.Authors.AddAsync(author);
+                await _db.SaveChangesAsync();
+
+                return await _db.Authors.FirstOrDefaultAsync(a => a.Name == author.Name);
+            }
+
+            return existAuthor;
+        }
+    }
+}
