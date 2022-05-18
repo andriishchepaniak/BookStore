84771dcce3d89e5ec2570d3c54438ba19bf5147b
andriishchepaniak <andrew.shtchepanyak@gmail.com>
2022-05-18 20:55:33 +0300

    Added producer and consumer services.

---
+++ BookStoreProducer/BookClubSettings.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BookStoreProducer
+{
+    public class BookClubSettings
+    {
+        public string BaseUrl { get; set; }
+        public List<string> Categories { get; set; }
+    }
+}
